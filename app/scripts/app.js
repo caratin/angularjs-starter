c67779e8f5be2a1e817183d995605e66458e10e2
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2017-09-29 12:12:59 +0100

    Uremoved bootstrap 3.3 and add new bootstrap 4-beta

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,36 +9,10 @@
       'ngSanitize',
       'ngTouch',
       'ngProgress',
-      'ui.router',
-      'ui.bootstrap',
+      'ui.router'
     ])
-    .constant('constants', {
-      'version': {
-        'number': '1.0.0'
-      },
-      'env': {
-        'local': {
-          'url': 'http://localhost:1234',
-          'isDebug': true
-        },
-        'dev': {
-          'url': 'http://localhost:1235',
-          'isDebug': true
-        },
-        'qa': {
-          'url': 'http://localhost:1236',
-          'isDebug': false
-        },
-        'uat': {
-          'url': 'http://localhost:1237',
-          'isDebug': false
-        },
-        'prod': {
-          'url': 'http://localhost:1238',
-          'isDebug': false
-        },
-        'current': 'local'
-      }
+    .constant('version', {
+      'number': '1.0.0'
     })
     .config(function ($httpProvider) {
       $httpProvider.interceptors.push('authInterceptor');
